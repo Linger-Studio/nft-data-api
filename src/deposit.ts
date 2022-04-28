5628931d04c40e20fb2436da7ee1e647cd54bad7
ziinxed <ziinxed@gmail.com>
2022-04-28 17:46:37 +0900

    base template fin

--- src/deposit.ts
+++ src/deposit.ts
@@ -1,13 +1,55 @@
-import { PublicKey } from "@solana/web3.js";
-import fetch from "node-fetch";
+import { Keypair, PublicKey, Signer } from "@solana/web3.js";
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
 
-export async function metadata(
+interface DepositInput {
+  customerPrivateKey: number[];
+  nftKey: string;
+}
+
+interface TxResult {
+  txHash: string;
+  state: boolean;
+  error?: string;
+}
+
+export async function deposit(
   event: APIGatewayProxyEventV2
 ): Promise<APIGatewayProxyResultV2> {
   const body = JSON.parse(event.body ?? "");
-  return {
-    statusCode: 200,
-    body: JSON.stringify(""),
-  };
+  const depositInfo: DepositInput = body.depositInfo;
+  const OPERATOR_ACCOUNT_PRIVATEKEY: string | undefined =
+    process.env.OPERATOR_ACCOUNT;
+
+  if (depositInfo === undefined) {
+    return {
+      statusCode: 403,
+    };
+  }
+
+  if (OPERATOR_ACCOUNT_PRIVATEKEY === undefined) {
+    return {
+      statusCode: 503,
+    };
+  }
+
+  try {
+    const customerAccount: Signer = Keypair.fromSecretKey(
+      Uint8Array.from(depositInfo.customerPrivateKey)
+    );
+
+    const operatorAccount: Signer = Keypair.fromSecretKey(
+      Uint8Array.from(JSON.parse(OPERATOR_ACCOUNT_PRIVATEKEY))
+    );
+
+    const nftKey: PublicKey = new PublicKey(depositInfo.nftKey);
+  } catch {
+    return {
+      statusCode: 403,
+    };
+  } finally {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(""),
+    };
+  }
 }
